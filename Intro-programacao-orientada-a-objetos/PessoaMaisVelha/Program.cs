630f0c74c731478072a707942a3aa9055708602a
Mayara Abreu <mayara.arj@gmail.com>
2022-06-21 11:14:10 -0300

    Adição dos exercícios de Introdução à Programação Orientada a Objetos

---
+++ Intro-programacao-orientada-a-objetos/PessoaMaisVelha/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace PessoaMaisVelha
+{
+    internal class Program
+    //Fazer um programa para ler os dados de duas pessoas, depois mostrar o nome da pessoa mais  velha. 
+
+    {
+        static void Main(string[] args)
+        {
+
+            Pessoa primeiraPessoa, segundaPessoa;
+            primeiraPessoa = new Pessoa();
+            segundaPessoa = new Pessoa();
+
+            Console.WriteLine("Dados da primeira pessoa:");
+            Console.Write("Nome: ");
+            primeiraPessoa.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            primeiraPessoa.Idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da segunda pessoa:");
+            Console.Write("Nome: ");
+            segundaPessoa.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            segundaPessoa.Idade = int.Parse(Console.ReadLine());
+
+
+
+            if (primeiraPessoa.Idade > segundaPessoa.Idade)
+            {
+                Console.WriteLine($"Pessoa mais velha: {primeiraPessoa.Nome}");
+            } else if(primeiraPessoa.Idade < segundaPessoa.Idade)
+            {
+                Console.WriteLine($"Pessoa mais velha: {segundaPessoa.Nome}");
+            } else
+            {
+                Console.WriteLine("As pessoas tem a mesma idade");
+            }
+
+
+
+
+        }
+    }
+}
---
+++ Intro-programacao-orientada-a-objetos/SalarioMedio/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace SalarioMedio
+{
+    internal class Program
+    //Fazer um programa para ler nome e salário de dois funcionários. Depois, mostrar o salário  médio dos funcionários.
+    {
+        static void Main(string[] args)
+        {
+            Funcionario primeiro, segundo;
+            primeiro = new Funcionario();
+            segundo = new Funcionario();
+
+            Console.WriteLine("Dados do primeiro funcionário:");
+            Console.Write("Nome: ");
+            primeiro.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            primeiro.Salario = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Dados do segundo funcionário:");
+            Console.Write("Nome: ");
+            segundo.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            segundo.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+
+
+            double mediaSalarial = (primeiro.Salario + segundo.Salario)/2;
+            Console.WriteLine("Salário médio: " + mediaSalarial.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}

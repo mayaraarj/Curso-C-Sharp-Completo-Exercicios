c3e4b4b6e964e5ab50dc8623950d4a24a9aac36a
Mayara Abreu <mayara.arj@gmail.com>
2022-06-23 10:51:57 -0300

    Adição do exercício AumentandoSalario

---
+++ Lista-exercicios-fixacao-programacao-orientada-objetos/AumentandoSalario/Funcionario.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace AumentandoSalario
+{
+    internal class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            double SalarioLiquido = SalarioBruto - Imposto;
+            return SalarioLiquido;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto = SalarioBruto + (porcentagem * SalarioBruto) / 100;
+        }
+
+
+        public override string ToString()
+        {
+            return Nome
+                + ", $ "
+                + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+    }
+}
---
+++ Lista-exercicios-fixacao-programacao-orientada-objetos/AumentandoSalario/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace AumentandoSalario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario func = new Funcionario();
+
+            Console.Write("Nome: ");
+            func.Nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            func.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture );
+            Console.Write("Imposto: ");
+            func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Funciononário: "+ func);
+            
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            func.AumentarSalario(porcentagem);
+            
+            Console.WriteLine("Dados atualizados: "+ func);
+          
+        }
+    }
+}

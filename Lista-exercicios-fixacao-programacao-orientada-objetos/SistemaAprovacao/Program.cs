4eee435255995878c10c2ba61a3d0b69aa989025
Mayara Abreu <mayara.arj@gmail.com>
2022-06-23 11:13:52 -0300

    Início do exercício SistemaAprovacao

---
+++ Lista-exercicios-fixacao-programacao-orientada-objetos/SistemaAprovacao/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace SistemaAprovacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno student = new Aluno();
+            
+            Console.Write("Nome do Aluno: ");
+            student.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno: ");
+            student.nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            student.nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            student.nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); 
+
+
+
+
+        }
+    }
+}

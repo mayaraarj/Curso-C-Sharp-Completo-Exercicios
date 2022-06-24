bd0dbd8154b1eb75801ee734f2e403ab5b8842c2
Mayara Abreu <mayara.arj@gmail.com>
2022-06-24 11:33:27 -0300

    Adicionei o início do exercício de classes estáticas (CambioIOF)

---
+++ Lista-exercicios-fixacao-programacao-orientada-objetos/CambioIOF/ConversorMoeda.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CambioIOF
+{
+    internal class ConversorMoeda
+    {
+        public static double Iof = 0.06;
+
+        public static double DolarParaReal(double dolaresComprados, double cotacaoDolar)
+        {
+            double total = dolaresComprados * cotacaoDolar;
+            return total + total * Iof;
+        }
+    }
+}

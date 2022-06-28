2a0a010eb8e901a48d187a2a488db0ea593b4dc3
Mayara Abreu <mayara.arj@gmail.com>
2022-06-28 10:32:28 -0300

    Tentativa de implementação do sistema bancário
    Construí o esqueleto da classe ContaBancaria; Inicializei a sobrecarga dos construtores dos dois tipos de conta: com e sem saldo inicial; Implementei operações de depósito e saque segundo as regras do negócio; pesquisando propriedades para implementar a obtenção e definição de valores, que podem ser propriedades auto-implementadas.

---
+++ ContaDoBanco/ContaBancaria.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class ContaBancaria
+{
+    public int Numero
+    { get; set; }
+    string Titular
+    { get; set; }
+
+    double Saldo;
+
+
+    public ContaBancaria(int numero, string titular, double saldo)
+    {
+        Numero = numero;
+        Titular = titular;
+        Saldo = saldo;
+    }
+
+    public ContaBancaria(int numero, string titular)
+    {
+        Numero = numero;
+        Titular = titular;
+        Saldo = 0;
+    }
+
+    public void Deposito(double quantia)
+    {
+        Saldo += quantia;
+    }
+
+    public void Saque(double quantia)
+    {
+        Saldo = Saldo - quantia - 5.00;
+    }
+
+}
---
+++ ContaDoBanco/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ContaDoBanco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta1 = new ContaBancaria();
+
+
+            Console.Write("Entre o número da conta: ");
+            
+        }
+    }
+}

f54bd47e604abad7b004c333753c6024af2f7d9e
Mayara Abreu <mayara.arj@gmail.com>
2022-07-06 10:21:50 -0300

    Início do exercício Pensionato
    A dona de um pensionato possui dez quartos para alugar para estudantes,
    sendo esses quartos identificados pelos números 0 a 9.
    Quando um estudante deseja alugar um quarto, deve-se registrar o nome
    e email deste estudante.
    Fazer um programa que inicie com todos os dez quartos vazios, e depois
    leia uma quantidade N representando o número de estudantes que vão
    alugar quartos (N pode ser de 1 a 10). Em seguida, registre o aluguel dos
    N estudantes. Para cada registro de aluguel, informar o nome e email do
    estudante, bem como qual dos quartos ele escolheu (de 0 a 9). Suponha
    que seja escolhido um quarto vago. Ao final, seu programa deve imprimir
    um relatório de todas ocupações do pensionato, por ordem de quarto,
    conforme exemplo.

---
+++ Pensionato/Aluguel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pensionato
+{
+    public class Aluguel
+    {
+        public Aluguel (string Nome, string Email, int Quarto)
+        {
+            Nome = nome;
+            Email = email;
+            Quarto = quarto;
+        }
+    }
+}
---
+++ Pensionato/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Pensionato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos quartos serão alugados?");
+            int n = int.Parse(Console.ReadLine());
+
+            Aluguel[] vect = new Aluguel[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string nome = Console.ReadLine();
+                string email = Console.ReadLine();
+                int quarto = int.Parse(Console.ReadLine());
+                vect[i] = new Aluguel(nome, email, quarto);
+            }
+
+
+
+        }
+    }
+}
+
+//for (int i = 0; i < n; i++)
+//{
+//    string name = Console.ReadLine();
+//    double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+//    vect[i] = new Product { Name = name, Price = price };
+//}
+//double sum = 0.0;
+//for (int i = 0; i < n; i++)
+//{
+//    sum += vect[i].Price;
+//}
+//double avg = sum / n;
+//Console.WriteLine("AVERAGE PRICE = " + avg.ToString("F2", CultureInfo.InvariantCulture));

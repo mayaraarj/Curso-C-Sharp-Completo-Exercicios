1967fb86050b614af9474410180ead9fe09eabab
Mayara Abreu <mayara.arj@gmail.com>
2022-07-07 09:45:55 -0300

    Finalização do exercício Pensionato

--- Pensionato/Program.cs
+++ Pensionato/Program.cs
@@ -4,35 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Quantos quartos serão alugados?");
+            Estudante[] vect = new Estudante[10];
+
+            Console.Write("Quantos quartos serão alugados? ");
             int n = int.Parse(Console.ReadLine());
 
-            Aluguel[] vect = new Aluguel[n];
-
-            for (int i = 0; i < n; i++)
+            for (int i = 1; i <= n; i++)
             {
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel #{i}:");
+                Console.Write("Nome: ");
                 string nome = Console.ReadLine();
+                Console.Write("Email: ");
                 string email = Console.ReadLine();
+                Console.Write("Quarto: ");
                 int quarto = int.Parse(Console.ReadLine());
-                vect[i] = new Aluguel(nome, email, quarto);
+                vect[quarto] = new Estudante(nome, email);
             }
 
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados: ");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
 
-
+            }
         }
     }
 }
-
-//for (int i = 0; i < n; i++)
-//{
-//    string name = Console.ReadLine();
-//    double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-//    vect[i] = new Product { Name = name, Price = price };
-//}
-//double sum = 0.0;
-//for (int i = 0; i < n; i++)
-//{
-//    sum += vect[i].Price;
-//}
-//double avg = sum / n;
-//Console.WriteLine("AVERAGE PRICE = " + avg.ToString("F2", CultureInfo.InvariantCulture));

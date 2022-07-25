f49858a7c5fb8da0e229f3f0a4c700db76651802
Mayara Abreu <mayara.arj@gmail.com>
2022-07-25 10:28:02 -0300

    Início do exercício - criação da classe Employee e comecei a implementar a lógica do programa
    Fazer um programa para ler um número inteiro N e depois os dados (id, nome e salario) de
    N funcionários. Não deve haver repetição de id.
    Em seguida, efetuar o aumento de X por cento no salário de um determinado funcionário.
    Para isso, o programa deve ler um id e o valor X. Se o id informado não existir, mostrar uma
    mensagem e abortar a operação. Ao final, mostrar a listagem atualizada dos funcionários,
    conforme exemplos.
    Lembre-se de aplicar a técnica de encapsulamento para não permitir que o salário possa
    ser mudado livremente. Um salário só pode ser aumentado com base em uma operação de
    aumento por porcentagem dada.

---
+++ RegistrandoFuncionarios-Listas/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace RegistrandoFuncionarios_Listas
+{
+    class Employee
+    {
+       public int Id { get; set; }
+       public string Name { get; set; }
+       public double Salary { get; private set; }
+
+        public Employee (int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }   
+
+        public void increaseSalary(double percentage)
+        {
+            Salary += percentage/100 * Salary;
+        }
+
+        public override string ToString()
+        {
+            return Id + ", " + Name + ", " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ RegistrandoFuncionarios-Listas/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+namespace RegistrandoFuncionarios_Listas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many employees will be registered?");
+            int numberEmployee = int.Parse(Console.ReadLine());
+
+            List<string> Employee = new List<string>();
+
+         
+
+
+
+
+
+
+
+        }
+    }
+}

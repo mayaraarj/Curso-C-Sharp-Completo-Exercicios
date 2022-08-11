9557dae8a78255faf4221b61b1e412d5b903ebeb
Mayara Abreu <mayara.arj@gmail.com>
2022-08-11 10:37:46 -0300

    Adição do exercício de fixação de matriz

---
+++ Exercicio-Matriz/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Exercicio_Matriz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vect = Console.ReadLine().Split();
+            int m = int.Parse(vect[0]);
+            int n = int.Parse(vect[1]);
+
+            int[,] matrix = new int[m, n];
+
+            for (int i = 0; i < m; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if ( x == matrix[i, j])
+                    {
+                        Console.WriteLine("Position: "+ i +","+ j);
+                    }
+
+                    if (j > 0)
+                    {
+                        Console.WriteLine("Left: " + matrix[i, j - 1]);
+                    }
+
+                    if (i > 0)
+                    {
+                        Console.WriteLine("Up" +matrix[ i-1, j] );
+                    }
+
+                    if (j < n - 1)
+                    {
+                        Console.WriteLine("Right: "+ matrix[i,j+1]);
+                    }
+
+                    if (i < m-1)
+                    {
+                        Console.WriteLine("Down: " + matrix[i + 1, j]);
+                    }
+
+
+
+                }
+            }
+
+
+        }
+    }
+}

62e487e2c93ca586f6a4936f0f0996300ae95eb2
Mayara Abreu <93221609+mayaraarj@users.noreply.github.com>
2022-09-02 16:08:02 -0300

    Iniciei exercício

---
+++ EnumeracoesEComposicao/EnumeracoesEComposicao/Entitties/Order.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnumeracoesEComposicao.Entitties.Enum;
+using EnumeracoesEComposicao.Entitties;
+
+namespace EnumeracoesEComposicao.Entitties
+{
+    internal class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        public Client Client { get; set; }
+
+        public List<OrderItem> Items { get; set; }= new List<OrderItem>();
+
+        public Order()
+        {
+
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            OrderStatus = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double sum = 0;
+            foreach (OrderItem item in Items)
+            {
+                sum += item.SubTotal();
+            }
+            return sum;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("ORDER SUMMARY: ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+
+        }
+
+    }
+}
---
+++ EnumeracoesEComposicao/EnumeracoesEComposicao/Program.cs
@@ -0,0 +1,52 @@
+﻿using EnumeracoesEComposicao.Entitties;
+using EnumeracoesEComposicao.Entitties.Enum;
+
+namespace EnumeracoesEComposicao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter client data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Bith date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            Client client = new Client(name, email, birthDate);
+
+            Console.WriteLine("Enter order data: ");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Console.WriteLine("How many items to this order?");
+            int qtd = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= qtd; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data: ");
+                Console.Write("Product name: ");
+                name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine());
+                Product product = new Product(name, price);
+                Console.Write("Quantity: ");
+                qtd = int.Parse(Console.ReadLine());
+                OrderItem orderItem = new OrderItem(qtd, price, product);
+
+            }
+
+            Order order = new Order(DateTime.Now, status,client);
+
+            //Console.WriteLine("ORDER SUMMARY: ");
+
+         
+
+
+
+
+
+
+
+        }
+    }
+}

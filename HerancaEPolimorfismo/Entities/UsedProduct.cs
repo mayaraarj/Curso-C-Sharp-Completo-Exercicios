d6d571651ba50a8aecc952cb4eec14bcb24d651d
Mayara Abreu <93221609+mayaraarj@users.noreply.github.com>
2022-09-22 11:47:35 -0300

    Finalizando o exercício de herança e polimorfismo

---
+++ HerancaEPolimorfismo/Entities/UsedProduct.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace HerancaEPolimorfismo.Entities
+{
+    internal class UsedProduct :Product
+    {
+        public DateOnly ManufactureDate { get; set; }
+
+        public UsedProduct()
+        {
+
+        }
+
+        public UsedProduct(string name, double price, DateOnly manufactureDate): base(name, price)
+        {
+            ManufactureDate = manufactureDate;
+        }
+
+        public override string PriceTag()
+        {
+            return Name + " (used) $ "+ Price.ToString("F2", CultureInfo.InvariantCulture) +" (Manufacture date: " + ManufactureDate + ")";
+        }
+    }
+}
--- HerancaEPolimorfismo/Program.cs
+++ HerancaEPolimorfismo/Program.cs
@@ -1,10 +1,60 @@
-﻿namespace HerancaEPolimorfismo
+﻿using System.Globalization;
+using HerancaEPolimorfismo.Entities;
+
+namespace HerancaEPolimorfismo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Product> products = new List<Product>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Product #{i} data: ");
+                Console.Write("Common, used or imported (c/u/i)?: ");
+                char productType = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Price: ");
+                double productPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (productType == 'c')
+                {
+                    products.Add(new Product(productName, productPrice));
+
+                }
+                else if (productType == 'u')
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateOnly manufactureDate = DateOnly.Parse(Console.ReadLine());
+                    products.Add(new UsedProduct(productName, productPrice, manufactureDate));
+                }
+                else if (productType == 'i')
+                {
+                    Console.Write("Customs fee: ");
+                    double customsFee = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                    products.Add(new ImportedProduct(productName, productPrice, customsFee));
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid product type!");
+                }
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("PRICE TAGS");
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.PriceTag());
+            }
+
+
+
         }
     }
 }

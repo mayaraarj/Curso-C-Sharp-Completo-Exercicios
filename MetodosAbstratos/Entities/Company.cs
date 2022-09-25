36be673b37f4bfe780cbee0b3ca67fc63172a128
Mayara Abreu <mayara.arj@gmail.com>
2022-09-25 20:03:57 -0300

    Implementação do exercício de métodos abstratos

---
+++ MetodosAbstratos/Entities/Company.cs
@@ -0,0 +1,25 @@
+﻿namespace MetodosAbstratos.Entities
+{
+    public class Company : TaxPayer
+    {
+        public int NumberEmployees{ get; set; }
+
+        public Company(string name, double anualIncome, int numberEmployees) : base(name, anualIncome)
+        {
+            NumberEmployees = numberEmployees;
+        }
+
+        public override double Tax()
+        {
+           if (NumberEmployees >= 10) {
+                return 0.14 * AnualIncome;
+            }
+           else
+            {
+                return 0.16 * AnualIncome;
+            }
+        }
+
+
+    }
+}
---
+++ MetodosAbstratos/Entities/TaxPayer.cs
@@ -0,0 +1,16 @@
+﻿namespace MetodosAbstratos.Entities
+{
+    abstract public class TaxPayer
+    {
+        public string Name { get; set; }
+        public double AnualIncome { get; set; }
+
+        protected TaxPayer(string name, double anualIncome)
+        {
+            Name = name;
+            AnualIncome = anualIncome;
+        }
+
+        public abstract double Tax();
+    }
+}

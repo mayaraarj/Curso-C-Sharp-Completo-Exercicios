304c080d29298bb262e1b7f66e5be70684c3bef5
Mayara Abreu <93221609+mayaraarj@users.noreply.github.com>
2022-09-27 16:06:55 -0300

    Iniciando exercício de exceção

---
+++ TratamentoDeExcecoes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using TratamentoDeExcecoes.Entities;
+
+namespace TratamentoDeExcecoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter account data");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine();
+            Console.Write("Inicial Balance: ");
+            double balance = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.Write("Withdraw Limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Account account = new Account(number, holder, balance, withdrawLimit);
+
+            Console.WriteLine("");
+            Console.WriteLine("Enter amount for withdraw: ");
+            double amount = double.Parse(Console.ReadLine());
+            account.Withdraw(amount);
+            Console.WriteLine("New balance: " + balance);
+        }
+    }
+}
